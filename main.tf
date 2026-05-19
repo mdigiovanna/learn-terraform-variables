7fa225fa55278782855ea08eaac170c814333b13
mdigiovanna <michaeldigiovanna2@gmail.com>
2026-05-19 14:30:13 -0400

    Remove s3_bucket module

--- main.tf
+++ main.tf
@@ -131,10 +131,3 @@ module "ec2_instances" {
     environment = "development"
   }
 }
-
-module "s3_bucket" {
-source = "app.terraform.io/policy-as-code-training/s3-bucket-md/aws"
-version = "1.0.1"
-
-bucket_name = "bucket-md-05-19-2026"
-}
